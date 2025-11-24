2bfcfe1945f098f1b8407c26e13165cf4f51ba70
4shikmathew <ashikmathew937@gmail.com>
2025-11-24 21:53:49 +0530

    ch

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="33xMJIYLkQJfL55d1EwYyzgMS5pHiUanGDPY5B6f0M0"
+      />
+    </>
+  );
+}
